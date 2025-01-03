c3271c1a6adc618892c45b33319aaea7b24ee6eb
lavanya-072 <2311cs010700@mallareddyuniversity.ac.in>
2025-01-03 10:09:10 +0530

    Add files via upload

---
+++ demo6.java
@@ -0,0 +1,12 @@
+public class demo6 {
+    public static void main(String[] args) {
+        String  username = "Lavanya";
+        String password = "lavanya@123";
+        while(username == "Lavanya" && password == "lavanya@123"){
+            System.out.println("Hello I am Lavanya");
+            break;
+        }
+
+    }
+ 
+}
