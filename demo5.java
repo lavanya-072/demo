aca51585454f79594c7426fe9c712cc242f3a27d
lavanya-072 <2311cs010700@mallareddyuniversity.ac.in>
2025-01-03 09:55:55 +0530

    Add files via upload

---
+++ demo5.java
@@ -0,0 +1,16 @@
+public class demo5 {
+    public static void main(String[] args) {
+        String  username = "user001";
+        String password = "user_001";
+        while(username == "user001" && password == "user_001"){
+            System.out.println("Hello dear Happy new year");
+            break;
+        }
+
+    }
+ 
+        
+    }
+    
+    
+
