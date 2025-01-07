011b9435adc2ec105b7b2cda427ab7c0b203b279
lavanya-072 <2311cs010700@mallareddyuniversity.ac.in>
2025-01-07 10:19:01 +0530

    Add files via upload
    Increment operator

---
+++ increment.java
@@ -0,0 +1,11 @@
+public class increment {
+    public static void main(String[] args) {
+            int num = 5;
+            System.out.println("Postfix Increment: " + num++); 
+            System.out.println("After Postfix Increment: " + num); 
+            System.out.println("Prefix Increment: " + ++num); 
+    }
+}
+
+
+
