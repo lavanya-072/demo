b93eacf6a8d879911b4c3d37c079aa1822044794
lavanya-072 <2311cs010700@mallareddyuniversity.ac.in>
2025-01-07 10:18:31 +0530

    Add files via upload
    Decrement operator

---
+++ Decrement.java
@@ -0,0 +1,9 @@
+public class Decrement {
+    public static void main(String[] args) {
+        int num = 7;
+        System.out.println("Postfix Decrement: " + num--); 
+        System.out.println("After Postfix Decrement: " + num); 
+         System.out.println("Prefix Decrement: " + --num); 
+    }
+    
+}
