93723863f806282a78b0834fc0b23cd3835d61b1
lavanya-072 <2311cs010700@mallareddyuniversity.ac.in>
2025-01-28 22:19:32 +0530

    Delete Decrement.java

--- Decrement.java
+++
@@ -1,9 +0,0 @@
-public class Decrement {
-    public static void main(String[] args) {
-        int num = 7;
-        System.out.println("Postfix Decrement: " + num--); 
-        System.out.println("After Postfix Decrement: " + num); 
-         System.out.println("Prefix Decrement: " + --num); 
-    }
-    
-}
